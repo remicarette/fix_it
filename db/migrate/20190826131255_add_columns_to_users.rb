6a5652bdb2d90500ccb76263dc8fbf04d74dedf6
remi carette <carette.remi@gmail.com>
2019-08-26 15:13:42 +0200

    generate model users

---
+++ db/migrate/20190826131255_add_columns_to_users.rb
@@ -0,0 +1,11 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone, :string
+    add_column :users, :address, :string
+    add_column :users, :zip_code, :string
+    add_column :users, :city, :string
+    add_column :users, :user_type, :string
+  end
+end
