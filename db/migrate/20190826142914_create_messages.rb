8595b7220c2577e0ab771e4d59e31c9fffe60f6e
remi carette <carette.remi@gmail.com>
2019-08-26 16:30:53 +0200

    migrate model message

--- db/migrate/20190826142914_create_messages.rb
+++ db/migrate/20190826142914_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.text :content
-      t.references :sender, foreign_key: true
-      t.references :receiver, foreign_key: true
+      t.references :sender, foreign_key: { to_table: :users }
+      t.references :receiver, foreign_key: { to_table: :users }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_140045) do
+ActiveRecord::Schema.define(version: 2019_08_26_142914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 2019_08_26_140045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_equipements_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "sender_id"
+    t.bigint "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receiver_id"], name: "index_messages_on_receiver_id"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -79,6 +89,8 @@ ActiveRecord::Schema.define(version: 2019_08_26_140045) do
   add_foreign_key "bookings", "equipements"
   add_foreign_key "bookings", "users"
   add_foreign_key "equipements", "users"
+  add_foreign_key "messages", "users", column: "receiver_id"
+  add_foreign_key "messages", "users", column: "sender_id"
   add_foreign_key "reviews", "bookings"
   add_foreign_key "skills", "users"
 end
