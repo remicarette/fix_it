f3831cccb9486fdf1a882cd303b5ec6e30b7a118
remi carette <carette.remi@gmail.com>
2019-08-28 13:01:58 +0200

    setup ajax but not working yet

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module FixIt
   class Application < Rails::Application
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     config.generators do |generate|
           generate.assets false
           generate.helper false
@@ -22,3 +23,4 @@ module FixIt
     # the framework and any gems in your application.
   end
 end
+
