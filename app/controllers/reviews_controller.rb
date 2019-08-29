94e791504ce8d55303c759592e0e48a82a43e895
remi carette <carette.remi@gmail.com>
2019-08-29 16:21:25 +0200

    reviews ok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,8 @@
 class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
-    @booking = Booking.find(params[:id])
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
     if @review.save
       redirect_to booking_path(@booking)
     else
