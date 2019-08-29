f99a4976e85f8555d39ab2d86f94b1d1f3d10825
remi carette <carette.remi@gmail.com>
2019-08-29 16:40:30 +0200

    solved problem in migration

--- db/migrate/20190829135908_change_type_date_to_date_time_in_bookings.rb
+++ db/migrate/20190829135908_change_type_date_to_date_time_in_bookings.rb
@@ -1,6 +1,6 @@
 class ChangeTypeDateToDateTimeInBookings < ActiveRecord::Migration[5.2]
   def change
-    change_column :bookings, :begin, :dateTime
-    change_column :bookings, :end, :dateTime
+    change_column :bookings, :begin, :datetime
+    change_column :bookings, :end, :datetime
   end
 end
