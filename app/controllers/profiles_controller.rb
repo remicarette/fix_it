584ca4c9586937ed7c3e68d8fff619fbdd0c7ea5
Renald Hubert <renald.aqua@me.com>
2019-08-29 11:00:07 +0200

    home ok

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,15 @@
-class UsersController < ApplicationController
+class ProfilesController < ApplicationController
+  def index
+    if params[:query].present?
+      raise
+      sql_query = "zip_code ILIKE :query OR model ILIKE :query"
+      @User = User.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @user = User.all
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
