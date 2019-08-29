befdccc9ff3f41fd9d56e79ec18872ba4cad189a
remi carette <carette.remi@gmail.com>
2019-08-29 15:34:46 +0200

    yarn install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,4 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import { getChatbox } from '../components/refreshPage';
 
-getChatbox();
+// getChatbox();
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,6 +2,4 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
 
 flatpickr(".datepicker", {
-
-
 })
