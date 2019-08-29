a07e722700b2fecb5f4dcbac062428ed2285a312
remi carette <carette.remi@gmail.com>
2019-08-29 15:51:44 +0200

    flatpickr ok

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,17 @@
 import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
+import "flatpickr/dist/themes/material_green.css"; // Note this is important!
 
 flatpickr(".datepicker", {
+  dateFormat: "d/m/Y",
+
+  "disable": [
+    function(date) {
+        // return true to disable
+        return (date.getDay() === 0 || date.getDay() === 6);
+
+    }
+  ],
+    "locale": {
+      "firstDayOfWeek": 1 // start week on Monday
+    }
 })
