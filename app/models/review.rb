b94c836f3f0a7715af468177d7d70f867df09c6d
Moritz Michalak <moritzmichalak@gmx.de>
2019-08-30 15:36:48 +0200

    f draft show pro edited

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :booking
 
+  def convert_date(date_at)
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    datum = date_at.to_s
+    year = datum[0..3]
+    month = months[datum[5..6].to_i - 1]
+    day = datum[8..9]
+    return "#{day} #{month}, #{year}"
+  end
 
 end
