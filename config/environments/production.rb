f2389aceccb59bee7a4b5b843836bada1d948662
remi carette <carette.remi@gmail.com>
2019-09-04 19:01:48 +0200

    action cable ready for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,9 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.action_cable.url = "wss://www.yourdomain.top/cable"
+  config.action_cable.allowed_request_origins = [ "http://www.yourdomain.top", "https://www.yourdomain.top" ]
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
