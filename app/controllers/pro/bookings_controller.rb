9fcd21261f6411f3bc0736982ef7d1a9a474adcb
remi carette <carette.remi@gmail.com>
2019-09-04 14:06:12 +0200

    filter bookings pro with status 4

--- app/controllers/pro/bookings_controller.rb
+++ app/controllers/pro/bookings_controller.rb
@@ -2,7 +2,7 @@ class Pro::BookingsController < ApplicationController
 
   def index
     if current_user.user_type == "pro"
-      @bookings = Booking.where(user_id: current_user.id).order(begin: :desc)
+      @bookings = Booking.where(user_id: current_user.id).where.not(status: 4).order(begin: :desc)
     else
       redirect_to root_path
     end
