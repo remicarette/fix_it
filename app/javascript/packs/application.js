00f651d240b03c413d675e415d9f37d770b83efd
remi carette <carette.remi@gmail.com>
2019-09-04 12:59:09 +0200

    interface ready to demoday

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,7 @@ import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/init_select2';
 import { displayReviews } from '../components/display_all_reviews'
 
-
 updateStars();
 getChatbox();
 initSelect2();
 displayReviews();
-
-
