e88e6ea27e7fa2a4279cd3602f2de4175e60c990
remi carette <carette.remi@gmail.com>
2019-09-04 10:20:57 +0200

    index & show actions created

--- app/controllers/pro/bookings_controller.rb
+++ app/controllers/pro/bookings_controller.rb
@@ -1,2 +1,11 @@
 class Pro::BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.where(user_id: current_user.id)
+  end
+
+  def show
+    @booking = Booking.where(params[:id])
+  end
+
 end
