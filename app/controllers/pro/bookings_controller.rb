d4bddc53ac45ffbf7aa5a7b1927ab07496cd9f61
remi carette <carette.remi@gmail.com>
2019-09-04 10:32:22 +0200

    index pro bookings ok

--- app/controllers/pro/bookings_controller.rb
+++ app/controllers/pro/bookings_controller.rb
@@ -1,11 +1,18 @@
 class Pro::BookingsController < ApplicationController
 
   def index
-    @bookings = Booking.where(user_id: current_user.id).reverse
+    if current_user.user_type == "pro"
+      @bookings = Booking.where(user_id: current_user.id).reverse
+    else
+      redirect_to root_path
+    end
   end
 
   def show
-    @booking = Booking.where(params[:id])
+    if current_user.user_type == "pro"
+      @booking = Booking.where(params[:id])
+    else
+      redirect_to root_path
+    end
   end
-
 end
