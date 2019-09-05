3797d92b08bf7372b1403daef8304728d3c95789
remi carette <carette.remi@gmail.com>
2019-09-05 15:12:26 +0200

    permit parameters for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+
+
+
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :zip_code, :city])
+  end
+
 end
